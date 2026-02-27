6228b7612ef00f2e31d230b99effcab7f34487f7
Mounika <devimonikaallam123@gmail.com>
2026-02-27 19:38:18 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/home", (req, res) => {
-  const subjects = ["DEVOPS", "CC", "SPM", "CD", "CNS"];
+  const subjects = ["DEVOPS", "CC", "SPM", "CD", "CNS" ,"java" ,"maths","c"];
   res.render("sample.pug", { sub: subjects });
 });
 app.listen(3000, () => {
